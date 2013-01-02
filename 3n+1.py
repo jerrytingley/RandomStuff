ceeb742bfabbefe5fe54853e4557167aa8e4ed82
jerrytingley <jerry.tingely@gmail.com>
2013-01-01 19:43:36 -0500

    Update 3n+1.py

--- 3n+1.py
+++ 3n+1.py
@@ -5,10 +5,8 @@ even = lambda x: True if (x % 2) == 0 else False
 def three_n_plus_one(n):
     i = 0
     while n != 1:
-        if even(n):
-            n = int(n / 2)
-        else:
-            n = int((3 * n) + 1)
+        if even(n): n = int(n / 2)
+        else: n = int((3 * n) + 1)
         i += 1
         print "[",i,"]", " n = ", n
 
