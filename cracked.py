7fa2d790ad71170a4c8ec8200bb25da8c7b77772
Jerry Tingley <jerry.tingley@gmail.com>
2013-01-07 23:21:22 -0500

    Implementation of math functions from Cracked
    http://www.cracked.com/article_19360_7-questions-you-didnt-know-could-be-answered-with-math.html

---
+++ cracked.py
@@ -0,0 +1,10 @@
+from __future__ import division
+from math import pi
+
+perfect_pancakes = lambda h, d: (4*h)/(pi-(d/2))
+beer_goggle_thickness = lambda an, d, s, l, vo: (an**2)*(d*(s+1))/((l**(1/2))*vo**2)
+procrastination = lambda e, v, i, v: (e*v)/(i*d)
+perfect_buttocks = lambda s, c, b, f, t, v: ((s+c)*(b+f))/((t-v))
+scary_movie = lambda es, u, cs, t, a, dr, fs, tl, f, n, sin, s: ((es+u+cs+t)**2)+s+((tl+f)/2)+((a+dr+fs)/n)+(sin*(x-1))
+marriage_proposal = lambda a1, a2: ((a2-a1)*0.368)+a1
+happiness = lambda p, e, h: (5*e)+(3*h)
