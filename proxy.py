484dfebb604fc8d3eb68bb738a7a6b78b33617c0
jerrytingley <jerry.tingely@gmail.com>
2013-03-12 16:18:30 -0300

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,4 +1,20 @@
-proxy = urllib2.ProxyHandler({'http': '127.0.0.1'})
-opener = urllib2.build_opener(proxy)
-urllib2.install_opener(opener)
-urllib2.urlopen('http://www.google.com')
+def test_proxy(prxoy):
+  try:
+    proxy = urllib2.ProxyHandler({'http': str(proxy)})
+    opener = urllib2.build_opener(proxy)
+    urllib2.install_opener(opener)
+  except Exception:
+    return False
+  return True
+
+def load_proxy(file_name):
+  with open(file_name) as p:
+    proxies = []
+    for line in proxies:
+      if line.startswith('http://'):
+        line = line[7::]
+      if len(line) > 1:
+        port = line.split(':')[-1]
+      else:
+        port = 80
+    
